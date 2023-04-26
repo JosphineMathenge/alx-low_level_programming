043486a2676f27cd6d8c4fcb4bb24562d21a153a
JosphineMathenge <josphinemathenge114@gmail.com>
2023-04-26 13:22:52 -0700

    103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,31 +1,29 @@
 #include <stdio.h>
 
 /**
-  * main - Prints the first 52 fibonacci numbers
-  *
-  * Return: Nothing!
-  */
+ * main - Prints the sum of even-valued Fibonacci sequence
+ *        terms not exceeding 4000000.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-	int i = 0;
-	long j = 1, k = 2;
+	unsigned long fib1 = 0, fib2 = 1, fibsum;
+	float tot_sum;
 
-	while (i < 50)
+	while (1)
 	{
-		if (i == 0)
-			printf("%ld", j);
-		else if (i == 1)
-			printf(", %ld", k);
-		else
-		{
-			k += j;
-			j = k - j;
-			printf(", %ld", k);
-		}
+		fibsum = fib1 + fib2;
+		if (fibsum > 4000000)
+			break;
 
-		++i;
+		if ((fibsum % 2) == 0)
+			tot_sum += fibsum;
+
+		fib1 = fib2;
+		fib2 = fibsum;
 	}
+	printf("%.0f\n", tot_sum);
 
-	printf("\n");
 	return (0);
 }
