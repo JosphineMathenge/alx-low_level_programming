f7bd918fd14d7f3dcf66eb33bf9c97053ea7e7ca
JosphineMathenge <josphinemathenge114@gmail.com>
2023-05-17 02:20:48 -0700

    Task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,46 +1,16 @@
-#include "main.h"
+#include "main.h"		
+#include <unistd.h>
 		
-#include <stdlib.h>
-		
-/**
-		
- * create_array - create array of size size and assign char c
-		
- * @size: size of array
-		
- * @c: char to assign
-		
- * Description: creat array of size size and assign char c
-		
- * Return: pointer to array, NULL if fail
-		
- *
-		
- */
-		
-char *create_array(unsigned int size, char c)
-		
-{
-		
-	char *str;
-		
-	unsigned int i;
-		
-
-		
-	str = malloc(sizeof(char) * size);
-		
-	if (size == 0 || str == NULL)
-		
-		return (NULL);
-		
-
-		
-	for (i = 0; i < size; i++)
-		
-		str[i] = c;
-		
-	return (str);
+/**		
+ * _putchar - writes the character c to stdout		
+* @c: The character to print		
+ *		
+ * Return: On success 1.		
+ * On error, -1 is returned, and errno is set appropriately.		
+ */		
+int _putchar(char c)		
+{	
+	return (write(1, &c, 1));
 		
 }
 
